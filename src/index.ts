6c4ab1f97fd2d6e66476d887f0b5d65576da4000
Cody Thompson <cody.thompson22@gmail.com>
2020-05-01 14:21:10 -0700

    switching computers commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,57 @@ type RenderArgs = {
   height: number
 }
 
+class ParsedLine {
+  lineNumber: number = NaN
+  args: number[] = []
+
+  get x(): number {
+    return this.args.length >= 1? this.args[0] : NaN
+  }
+
+  get y(): number {
+    return this.args.length >= 2? this.args[1] : NaN
+  }
+
+  get w(): number {
+    return this.args.length >= 3? this.args[2] : NaN
+  }
+
+  get h(): number {
+    return this.args.length >= 3? this.args[2] : NaN
+  }
+}
+
+class DrawCommand extends ParsedLine {
+  lineNumber: number
+  args:number[]
+
+  // TODO constructor - then Routine?
+}
+
+type ParseResult = {
+  errors: string[]
+  lines: ParsedLine[]
+}
+
+function parseLines(content:string): ParseResult {
+  const errors:string[] = []
+  const parsedLines:ParsedLine[] = []
+  const lines:string[][] = content
+    .split("\n")
+    .map(line => line.trim().split(" "))
+  for (let i = 0; i < lines.length; i++) {
+    const tokens:string[] = lines[i]
+    if (tokens.length == 0) {
+      continue
+    }
+  }
+  return {
+    errors,
+    lines: parsedLines
+  }
+}
+
 export function render(renderArgs:RenderArgs): string {
   const {width, height} = renderArgs
   return (`
@@ -17,11 +68,11 @@ async function go():Promise<void> {
   const tst:string = render({width: 128, height: 128})
   const html:string = `
     <!DOCTYPE html>
-    <html style="width: 100%; height: 100%;">
+    <html">
     <head>
       <title>lkjlkj</title>
     </head>
-    <body style="width: 100%; height: 100%; background-color: #222">
+    <body style="background-color: #222">
       <img src="test.svg" />
       <hr />
       ${tst}
